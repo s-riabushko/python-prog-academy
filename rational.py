59dee9be68cb9679330fce3ebe7454b47f1bdaff
s-riabushko <s.riabushko@gmail.com>
2024-07-15 20:05:32 +0300

    HW 05 Task 3 fixes

--- rational.py
+++ rational.py
@@ -5,94 +5,174 @@ class Rational:
     """
     Class Rational for compare, addition, multiplication and subtraction of simple fractions
     """
-    def __init__(self, a: int, b: int):
-        """
-        :param a: numerator (integer)
-        :param b: denominator (positive integer)
-        """
-        if not isinstance(a | b, int):
-            raise TypeError('numerator and denominator must be an integer')
-        if b == 0:
-            raise ZeroDivisionError('Denominator must not equal zero')
-        self.__gcd = math.gcd(a, b)
-        self.a = a // self.__gcd
-        self.b = b // self.__gcd
+    def __init__(self, numerator: int, denominator: int):
+        if not isinstance(numerator, int):
+            raise TypeError(f"Numerator must be integers, got {type(numerator).__name__}.")
+        if not isinstance(denominator, int):
+            raise TypeError(f"Denominator must be integers, got {type(denominator).__name__}.")
+        if denominator == 0:
+            raise ZeroDivisionError("Denominator cannot be zero.")
+        self.numerator = numerator
+        self.denominator = denominator
 
     def __eq__(self, other):
-        if not isinstance(other, Rational):
+        if isinstance(other, int):
+            other = Rational(other, 1)
+        elif isinstance(other, tuple) and len(other) == 2:
+            other = Rational(other[0], other[1])
+        elif not isinstance(other, Rational):
             return NotImplemented
-        return self.a == other.a and self.b == other.b
+        return self.numerator * other.denominator == other.numerator * self.denominator
 
     def __ne__(self, other):
-        if not isinstance(other, Rational):
+        if isinstance(other, int):
+            other = Rational(other, 1)
+        elif isinstance(other, tuple) and len(other) == 2:
+            other = Rational(other[0], other[1])
+        elif not isinstance(other, Rational):
             return NotImplemented
-        return self.a != other.a or self.b != other.b
+        return self.numerator * other.denominator != other.numerator * self.denominator
 
     def __lt__(self, other):
-        if not isinstance(other, Rational):
+        if isinstance(other, int):
+            other = Rational(other, 1)
+        elif isinstance(other, tuple) and len(other) == 2:
+            other = Rational(other[0], other[1])
+        elif not isinstance(other, Rational):
             return NotImplemented
-        if self.a / self.b < 0 <= other.a / other.b:
-            return True
-        if self.a / self.b >= 0 > other.a / other.b:
+        if self.numerator / self.denominator < 0 <= other.numerator / other.denominator:
             return False
-        return abs(self.a) * abs(other.b) < abs(other.a) * abs(self.b)
+        if other.numerator / other.denominator < 0 <= self.numerator / self.denominator:
+            return True
+        return self.numerator * other.denominator < other.numerator * self.denominator
 
     def __le__(self, other):
-        if not isinstance(other, Rational):
+        if isinstance(other, int):
+            other = Rational(other, 1)
+        elif isinstance(other, tuple) and len(other) == 2:
+            other = Rational(other[0], other[1])
+        elif not isinstance(other, Rational):
             return NotImplemented
-        if self.a / self.b < 0 <= other.a / other.b:
-            return True
-        if self.a / self.b >= 0 > other.a / other.b:
+        if self.numerator / self.denominator < 0 <= other.numerator / other.denominator:
             return False
-        return self.a * other.b <= other.a * self.b
+        if other.numerator / other.denominator < 0 <= self.numerator / self.denominator:
+            return True
+        return self.numerator * other.denominator <= other.numerator * self.denominator
 
     def __gt__(self, other):
-        if not isinstance(other, Rational):
+        if isinstance(other, int):
+            other = Rational(other, 1)
+        elif isinstance(other, tuple) and len(other) == 2:
+            other = Rational(other[0], other[1])
+        elif not isinstance(other, Rational):
             return NotImplemented
-        if self.a / self.b < 0 <= other.a / other.b:
-            return False
-        if self.a / self.b >= 0 > other.a / other.b:
+        if self.numerator / self.denominator >= 0 > other.numerator / other.denominator:
             return True
-        return self.a * other.b > other.a * self.b
+        if other.numerator / other.denominator >= 0 > self.numerator / self.denominator:
+            return False
+        return self.numerator * other.denominator > other.numerator * self.denominator
 
     def __ge__(self, other):
-        if self.a / self.b < 0 <= other.a / other.b:
-            return False
-        if self.a / self.b >= 0 > other.a / other.b:
-            return True
-        if not isinstance(other, Rational):
+        if isinstance(other, int):
+            other = Rational(other, 1)
+        elif isinstance(other, tuple) and len(other) == 2:
+            other = Rational(other[0], other[1])
+        elif not isinstance(other, Rational):
             return NotImplemented
-        return self.a * other.b >= other.a * self.b
+        if self.numerator / self.denominator >= 0 > other.numerator / other.denominator:
+            return True
+        if other.numerator / other.denominator >= 0 > self.numerator / self.denominator:
+            return False
+        return self.numerator * other.denominator >= other.numerator * self.denominator
 
     def __add__(self, other):
-        if not isinstance(other, Rational):
+        if isinstance(other, int):
+            other = Rational(other, 1)
+        elif isinstance(other, tuple) and len(other) == 2:
+            other = Rational(other[0], other[1])
+        elif not isinstance(other, Rational):
             return NotImplemented
-        num = self.a * other.b + other.a * self.b
-        denom = self.b * other.b
-        return Rational(num, denom)
+        new_numerator = self.numerator * other.denominator + other.numerator * self.denominator
+        new_denominator = self.denominator * other.denominator
+        return Rational(new_numerator, new_denominator)
+
+    def __radd__(self, other):
+        return self + other
+
+    def __iadd__(self, other):
+        tmp = self + other
+        self.numerator = tmp.numerator
+        self.denominator = tmp.denominator
+        return self
 
     def __sub__(self, other):
-        if not isinstance(other, Rational):
+        if isinstance(other, int):
+            other = Rational(other, 1)
+        elif isinstance(other, tuple) and len(other) == 2:
+            other = Rational(other[0], other[1])
+        elif not isinstance(other, Rational):
             return NotImplemented
-        num = self.a * other.b - other.a * self.b
-        denom = self.b * other.b
-        return Rational(num, denom)
+        new_numerator = self.numerator * other.denominator - other.numerator * self.denominator
+        new_denominator = self.denominator * other.denominator
+        return Rational(new_numerator, new_denominator)
+
+    def __rsub__(self, other):
+        return self - other
+
+    def __isub__(self, other):
+        tmp = self - other
+        self.numerator = tmp.numerator
+        self.denominator = tmp.denominator
+        return self
 
     def __mul__(self, other):
-        if not isinstance(other, Rational):
+        if isinstance(other, int):
+            other = Rational(other, 1)
+        elif isinstance(other, tuple) and len(other) == 2:
+            other = Rational(other[0], other[1])
+        elif not isinstance(other, Rational):
             return NotImplemented
-        num = self.a * other.a
-        denom = self.b * other.b
-        return Rational(num, denom)
+        new_numerator = self.numerator * other.numerator
+        new_denominator = self.denominator * other.denominator
+        return Rational(new_numerator, new_denominator)
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __imul__(self, other):
+        tmp = self * other
+        self.numerator = tmp.numerator
+        self.denominator = tmp.denominator
+        return self
 
     def __str__(self):
-        if self.a == 0:
+        if self.numerator == 0:
             return '0'
-        if self.a == self.b:
+        if self.numerator == self.denominator:
             return '1'
-        int_part = abs(self.a) // self.b
-        if int_part:
-            a = abs(self.a) % self.b
-            return f"{'-' if self.a < 0 else ''}{int_part} {a}/{self.b}"
-        else:
-            return f"{self.a}/{self.b}"
+        gcd = math.gcd(self.numerator, self.denominator)
+        tmp_numerator = abs(self.numerator // gcd)
+        tmp_denominator = abs(self.denominator // gcd)
+        if tmp_denominator == 1:
+            return f'{tmp_numerator}'
+        sign = '-' if self.numerator * self.denominator < 0 else ''
+        if tmp_numerator > tmp_denominator:
+            whole = tmp_numerator // tmp_denominator
+            tmp_numerator = tmp_numerator % tmp_denominator
+            return f'{sign}{whole} {tmp_numerator}/{tmp_denominator}'
+
+        return f'{sign}{tmp_numerator}/{tmp_denominator}'
+
+
+if __name__ == '__main__':
+    # HW 05 Task 3
+    print("\nTask 3")
+    x = Rational(3, 11)
+    y = Rational(7, -8)
+    print(f"x = {x}")
+    print(f"y = {y}")
+    print(f"x > y: {x > y}")
+    print(f"x + y: {x + y}")
+    print(f"x - y: {x - y}")
+    print(f"y - x: {y - x}")
+    print(f"x * y: {x * y}")
